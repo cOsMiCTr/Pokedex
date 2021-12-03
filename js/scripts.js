c7473bf3c2cf124b159d0c976e4b8de334c8587a
cOsMiCTr <onurbaki@me.com>
2021-12-03 13:47:02 +0100

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+
+
+// Variables try-outs
+
+/* let myName = 'Bob';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName);
+ */
+
+
+
+
+
+// Operators
+
+
+/* let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize); */
+
+
+/* let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
+ */
+
+alert('Hello world')
+
+let favoriteFood = 'Pizza';
+
+document.write(favoriteFood);
