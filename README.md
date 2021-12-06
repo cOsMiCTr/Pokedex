e1a323a2079d2090007686def87c66361bf15b2c
cOsMiCTr <onurbaki@me.com>
2021-12-07 00:11:34 +0100

    First Pokemon list has been generated

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 
+Project description
+How to get the project running
+Project dependencies (e.g., JavaScript version, ESLint rules)
+Which API the project uses
---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+// Pokemon names
+
+let pokemonList = [
+  {name: 'Charmander', height:0.6, types:['Fire']},
+  {name: 'Charmeleon', height:1.1, types:['Fire']},
+  {name: 'Charizard', height:1.7, types:['Fire','Flying']},
+  {name: 'Onix', height:8.8, types:['Rock','Ground']},
+  {name: 'Mr. Mime', height:1.3, types:['Psychic','Fairy']},
+  {name: 'Snorlax', height:2.1, types:['Normal']}
+];
+
+// Print it on the screen
+
+document.write(pokemonList[4].types);
