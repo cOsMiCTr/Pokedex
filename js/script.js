5544b0a6af69fe6afe033e03b617a23c501f07df
cOsMiCTr <onurbaki@me.com>
2021-12-09 23:22:28 +0100

    Task 1.4

--- js/script.js
+++ js/script.js
@@ -5,6 +5,9 @@ let pokemonList = [
   {name: 'Charmander', height:0.6, types:['Fire']},
   {name: 'Charmeleon', height:1.1, types:['Fire']},
   {name: 'Charizard', height:1.7, types:['Fire','Flying']},
+];
+
+let pokemonList2 = [
   {name: 'Onix', height:8.8, types:['Rock','Ground']},
   {name: 'Mr. Mime', height:1.3, types:['Psychic','Fairy']},
   {name: 'Snorlax', height:2.1, types:['Normal']}
@@ -12,25 +15,35 @@ let pokemonList = [
 
 // Variables
 
-let pokemonNames = "";
-let heightHuge = "Wow, that’s big! ";
+let heightHuge = "Wow, that\’s big! ";
 let divPokemonList = "<div class=\"pokemon-list\">"
 let divClose = "</div>";
 let spanExcitement = "<span class=\"excitement\">";
 let spanClose = "</span>"
 
 // Print on screen
-
-for(i = 0; i < pokemonList.length; i++) {
-  pokemonList[i].height >= 2.0 ?
-  document.write(divPokemonList + pokemonList[i].name + " ( height : " + pokemonList[i].height + " ) - " + spanExcitement + heightHuge + spanClose + divClose):
-  document.write(divPokemonList + pokemonList[i].name + " ( height : " + pokemonList[i].height + " )" + divClose)
+/*
+function printArrayDetails(){
+  for(i = 0; i < pokemonList.length; i++) {
+    pokemonList[i].height >= 2.0 ?
+    document.write(divPokemonList + pokemonList[i].name + " ( height : " + pokemonList[i].height + " ) - " + spanExcitement + heightHuge + spanClose + divClose):
+    document.write(divPokemonList + pokemonList[i].name + " ( height : " + pokemonList[i].height + " )" + divClose)
+  }
 }
 
+printArrayDetails();*/
 
 
+function printArrayDetails(list){
+  for(i = 0; i < list.length; i++) {
+    list[i].height >= 2.0 ?
+    document.write(divPokemonList + list[i].name + " ( height : " + list[i].height + " ) - " + spanExcitement + heightHuge + spanClose + divClose):
+    document.write(divPokemonList + list[i].name + " ( height : " + list[i].height + " )" + divClose)
+  }
+}
 
-
+printArrayDetails(pokemonList);
+printArrayDetails(pokemonList2);
 
 /*
 let person = [
