b4e454cfecae4eca6418f7e037dd132b6e4125ec
cOsMiCTr <onurbaki@me.com>
2022-01-11 00:07:50 +0100

    new branch

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,8 @@
 
 const pokemonRepository = (() => {
   let modalContainer = document.querySelector(".modal");
-
+  let modalBody = $('.modal-body');
+  let modalTitle = $('.modal-title');
   // Pokemon names
   let pokemonList = [];
 
@@ -19,59 +20,24 @@ const pokemonRepository = (() => {
 
   let addListItem = (pokemon) => {
     // Adding pokemons
-    const pokemonListContainer = document.querySelector('.row');
-    let container = document.createElement('li'),
-        // pokemonCardContainer = document.createElement('div'),
-        pokemonCardImg = document.createElement('img'),
-        pokemonImg = document.createElement('img'),
-        pokemonName = document.createElement('p');
-
-        // pokemonCardContainer.classList.add('pokemon-card-container');
-        pokemonCardImg.classList.add('pokemon-card');
-        pokemonCardImg.setAttribute("src", "./img/pokemon-card.png")
-        pokemonCardImg.setAttribute("alt", "Pokemon Card");
-
-        pokemonImg.setAttribute("alt", "Pokemon image of " + pokemon.name);
-        pokemonImg.setAttribute("role", "image");
-        pokemonImg.classList.add("pokemon-image");
-
-        pokemonName.classList.add("pokemon-name");
-
-        pokemonName.innerText = pokemon.name;
-    
-    container.innerText.toUpperCase();
-    container.classList.add("list-group");
-    // Sizing of the Grid
-    container.classList.add("col-6");
-    container.classList.add("col-xs-5");
-    container.classList.add("col-sm-4");
-    container.classList.add("col-md-3");
-    container.classList.add("col-lg-2");
-    container.classList.add("pokemon-container");
-    container.setAttribute("data-toggle", "modal");
-    // Append the items
-    
-    container.appendChild(pokemonCardImg);
-    container.appendChild(pokemonName);
-    container.appendChild(pokemonImg);
-    // container.appendChild(pokemonCardContainer);
-    pokemonListContainer.appendChild(container);
+    $("ul.row").html('<li class="list-group col-6 col-xs-5 col-sm-4 col-md-3 col-lg-2 pokemon-container"' +
+    'data-toggle="modal"><p class="pokemon-name">' + pokemon.name + '</p>' + 
+    '<img class="pokemon-image" src="' + pokemon.image + '"></li>');
 
     // Return button test when clicked
     container.addEventListener("click", () => showDetails(pokemon));
 
     let url = pokemon.detailsUrl;
-    return fetch(url).then(function(response){
+    return fetch(url).then( (response) => {
       return response.json();
-    }).then(function(details){
+    }).then( (details) => {
       pokemon.image = details.sprites.front_default;
       pokemonImg.setAttribute ('src', pokemon.image);
-    }).catch(function(e){
+    }).catch( (e) => {
       console.error(e);
     });
 
   };
-
 
   let showDetails = (item) =>
     pokemonRepository
@@ -80,19 +46,18 @@ const pokemonRepository = (() => {
         showModal(item.name, item.imageUrl, item.height, item.weight, item.type)
       );
 
-  function loadList() {
+   let loadList = ()  => {
     return fetch('https://pokeapi.co/api/v2/pokemon/?limit=200').then(function (response) {
       return response.json();
-    }).then(function (json) {
-      json.results.forEach(function (item) {
+    }).then( (json) => {
+      json.results.forEach( (item) => {
         let pokemon = {
           name: item.name,
           detailsUrl: item.url
         };
         add(pokemon);
       });
-    }).catch(function (e) {
-      hideLoadingMessage();
+    }).catch( (e) => {
       console.error(e);
     })
   }
@@ -116,9 +81,6 @@ const pokemonRepository = (() => {
   }
 
   let showModal = (title, image, height, weight, type) => {
-    let modalBody = $('.modal-body');
-    let modalTitle = $('.modal-title');
-
     modalTitle.empty();
     modalBody.empty();
 
@@ -131,37 +93,24 @@ const pokemonRepository = (() => {
     let typeElement = $('<p>' + 'Type(s) : ' + type + '</p>');
 
     modalTitle.append(nameElement);
-    modalBody.append(imageElement);
-    modalBody.append(heightElement);
-    modalBody.append(weightElement);
-    modalBody.append(typeElement);
+    modalBody.append(imageElement).append(heightElement).append(weightElement).append(typeElement);
 
     $('#PokedexModal').modal();
   }
 
   let showContactModal = () => {
-    let modalBody = $('.modal-body');
-    let modalTitle = $('.modal-title');
-
     modalTitle.empty();
     modalBody.empty();
 
-    let nameElement = $('<h1 class="text-capitalize">Contact</h1>');
-
-    let emailElement = $('<label for="contact-email">Enter your Email address</label><input type="email" class="form-control" id="contact-email" aria-describedby="emailHelp" placeholder="Enter email">');
-
-    let messageElement = $('<label for="contact-textarea">Your message</label><textarea class="form-control" id="contact-textarea" rows="3"></textarea>')
-
-    let inputElement = $('<input class="btn btn-primary" type="submit" value="Submit">');
-
-
+    let nameElement = $('<h1 class="text-capitalize">Contact</h1>'),
+      emailElement = $('<label for="contact-email">Enter your Email address</label><input type="email" class="form-control" id="contact-email" aria-describedby="emailHelp" placeholder="Enter email">'),
+      messageElement = $('<label for="contact-textarea">Your message</label><textarea class="form-control" id="contact-textarea" rows="3"></textarea>'),
+      inputElement = $('<input class="btn btn-primary" type="submit" value="Submit">');
 
     modalBody.empty();
 
     modalTitle.append(nameElement);
-    modalBody.append(emailElement);
-    modalBody.append(messageElement);
-    modalBody.append(inputElement);
+    modalBody.append(emailElement).append(messageElement).append(inputElement);
 
     $('#ContactModal').modal();
   };
@@ -195,11 +144,6 @@ const pokemonRepository = (() => {
     }
   });
 
-
-  document
-    .querySelector("#PokedexModal")
-    .addEventListener("click", () => showModal);
-
   document
     .querySelector(".Contact")
     .addEventListener("click", () => showContactModal());
@@ -217,6 +161,6 @@ const pokemonRepository = (() => {
 })();
 
 
- pokemonRepository.loadList().then( () =>
+ pokemonRepository.loadList().then( () => 
   pokemonRepository.getAll().forEach( pokemon =>
     pokemonRepository.addListItem(pokemon)));
